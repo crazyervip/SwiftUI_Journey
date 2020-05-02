12b0d9f66c6124db56071fe5468c92fb29eef5cf
Jacob_d <zjc8966@me.com>
2020-05-02 18:34:40 +0800

    07

--- 07_Project05_WordScramble/07_Project05_WordScramble/ContentView.swift
+++ 07_Project05_WordScramble/07_Project05_WordScramble/ContentView.swift
@@ -7,20 +7,23 @@
 //
 
 import SwiftUI
+import Introspect
 
 struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
-
+    
     @State private var errorTitle = ""
     @State private var errorMessage = ""
     @State private var showingError = false
-
-    // challenge 1
+    
+    @State private var showingKeyboard = true
+    
+    //MARK: challenge 1
     private let minWordLength = 3
-
-    // challenge 3
+    
+    //MARK: challenge 3
     private var score: Int {
         var count = 0
         for word in usedWords {
@@ -28,99 +31,157 @@ struct ContentView: View {
         }
         return count
     }
-
+    
     var body: some View {
         NavigationView {
-            VStack {
-                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-                    .autocapitalization(.none)
-
-                List(usedWords, id: \.self) {
-                    // HStack is added automatically if there are several elements on the same row
-
-                    // add number of letters
-                    Image(systemName: "\($0.count).circle")
-                    Text($0)
+            ZStack {
+                //MARK: dark mode
+                //                Color("Background").edgesIgnoringSafeArea(.all)
+                VStack {
+                    LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)),Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1))]), startPoint: .bottomTrailing, endPoint: .topLeading)
+                        .opacity(0.3)
+                        .edgesIgnoringSafeArea(.all)
+                        .frame(height: 80)
+                    Spacer()
                 }
-
-                // challenge 3
-                Text("Score: \(score)")
-            }
-            .navigationBarTitle(rootWord)
-            // challenge 2
-            .navigationBarItems(leading: Button("New Game") { self.startGame() })
-            .onAppear(perform: startGame)
-            .alert(isPresented: $showingError) {
-                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+                VStack(spacing: 0) {
+                    HStack {
+                        HStack {
+                            Image(systemName: "square.and.pencil")
+                                .font(.system(size: 18, weight: .semibold))
+                                .padding(Edge.Set.leading, 7)
+                                .foregroundColor(Color(#colorLiteral(red: 0.3348660767, green: 0.3034154177, blue: 0.8043338656, alpha: 1)))
+                            
+                            TextField("这些字母打乱顺序还能组成什么？", text: $newWord, onCommit: addNewWord)
+                                .introspectTextField { tf in
+                                    tf.becomeFirstResponder()
+                            }
+                            .foregroundColor(.primary)
+                            .autocapitalization(.none)
+                            .disableAutocorrection(true)
+                            
+                            
+                            Button(action: {
+                                self.newWord = ""
+                            }) {
+                                Image(systemName: "xmark.circle.fill")
+                            }.accentColor(Color(#colorLiteral(red: 0.3348660767, green: 0.3034154177, blue: 0.8043338656, alpha: 1)))
+                        }
+                        .padding(EdgeInsets(top: 5, leading: 5, bottom: 4, trailing: 8))
+                            
+                            //MARK: 注意 cornerRadius 只能修饰背景，否则会阻止键盘 becomeFirstResponder
+                            .background(Color.white.opacity(0.8).cornerRadius(20))
+                        
+                    }
+                    .padding()
+                    
+                    List(usedWords, id: \.self) {
+                        // 自动规整为 HStack
+                        Image(systemName: "\($0.count).circle")
+                        Text($0)
+                    }
+                        // MARK: 滑动隐藏键盘
+                        .gesture(
+                            DragGesture()
+                                .onChanged {_ in
+                                    self.endEditing()
+                            }
+                    )
+                        .overlay(
+                            VStack {
+                                Text("注意每个字母只能用一次")
+                                    .foregroundColor(Color(.systemGray))
+                                    .padding()
+                                    .background(BlurBg(style: .systemUltraThinMaterial))
+                                    .cornerRadius(20)
+                                    .padding()
+                                    .offset(x: score > 0 ? 400 : 0)
+                                Spacer()
+                            }.animation(.spring())
+                    )
+                }
+                .edgesIgnoringSafeArea(.bottom)
+                .navigationBarTitle(rootWord)
+                    // challenge 2
+                    .navigationBarItems(leading: Button("换个单词") { self.startGame() }                                .foregroundColor(Color(#colorLiteral(red: 0.3348660767, green: 0.3034154177, blue: 0.8043338656, alpha: 1)))
+                        , trailing:
+                        //MARK: challenge 3
+                        Text("总分: \(score)")
+                            .font(.system(size: 20, weight: .black, design: .rounded)))
+                    .onAppear(perform: startGame)
+                    .alert(isPresented: $showingError) {
+                        Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("好的")))
+                }
             }
         }
     }
-
+    private func endEditing() {
+        UIApplication.shared.endEditing()
+    }
+    
     func startGame() {
-        // challenge 2
+        //MARK: challenge 2
         usedWords.removeAll()
-
+        
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 let allWords = startWords.components(separatedBy: "\n")
-
+                
                 rootWord = allWords.randomElement() ?? "silkworm"
-
+                
                 return
             }
         }
-
-        fatalError("Could not load start.txt from bundle.")
+        
+        fatalError("无法在 bundle 中找到文件.")
     }
-
+    
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-
+        
         guard answer.count > 0 else {
             return
         }
-
+        
         guard isOriginal(word: answer) else {
-            wordError(title: "Word used already", message: "Be more original")
+            wordError(title: "这个单词已经想到过了", message: "再好好想想吧")
             return
         }
-
+        
         guard isPossible(word: answer) else {
-            wordError(title: "Word not recognized", message: "You can't just make them up, you know")
+            wordError(title: "范围之外", message: "请使用标题的字母")
             return
         }
-
+        
         guard isReal(word: answer) else {
-            wordError(title: "Word not possible", message: "That isn't a real word")
+            wordError(title: "没有这个单词", message: "别瞎编哦")
             return
         }
-
-        // challenge 1
+        
+        //MARK: challenge 1
         guard isLongEnough(word: answer) else {
-            wordError(title: "Word too short", message: "Words must be \(minWordLength) letters at least")
+            wordError(title: "该单词太短了", message: "要求至少含有 \(minWordLength) 个字母")
             return
         }
-
-        // challenge 1
+        
+        //MARK: challenge 1
         guard isNotWordToGuess(word: answer) else {
-            wordError(title: "Incorrect word", message: "Word must be different than word to guess")
+            wordError(title: "标题不算", message: "再好好想想吧")
             return
         }
-
-        // insert on top of the list
+        
+        // 顶部插入新 cell
         usedWords.insert(answer, at: 0)
         newWord = ""
     }
-
+    
     func isOriginal(word: String) -> Bool {
         !usedWords.contains(word)
     }
-
+    
     func isPossible(word: String) -> Bool {
         var tempWord = rootWord.lowercased()
-
+        
         for letter in word {
             if let pos = tempWord.firstIndex(of: letter) {
                 tempWord.remove(at: pos)
@@ -129,28 +190,28 @@ struct ContentView: View {
                 return false
             }
         }
-
+        
         return true
     }
-
+    
     func isReal(word: String) -> Bool {
         let checker = UITextChecker()
         let range = NSMakeRange(0, word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-
+        
         return misspelledRange.location == NSNotFound
     }
-
-    // challenge 1
+    
+    //MARK: challenge 1
     func isLongEnough(word: String) -> Bool {
         return word.count >= minWordLength
     }
-
-    // challenge 1
+    
+    //MARK: challenge 1
     func isNotWordToGuess(word: String) -> Bool {
         return word != rootWord
     }
-
+    
     func wordError(title: String, message: String) {
         errorTitle = title
         errorMessage = message
@@ -161,5 +222,25 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        //         .colorScheme(.dark)
+    }
+}
+//MARK: 模糊背景
+
+struct BlurBg: UIViewRepresentable {
+    var style: UIBlurEffect.Style = .systemMaterial
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: style)
+    }
+}
+
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
